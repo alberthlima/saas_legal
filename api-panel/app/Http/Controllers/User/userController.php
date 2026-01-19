d7c5e42af51a0c1f133ae68d61df9af068ffba4e
Alberth Lima <araellima@gmail.com>
2026-01-19 09:15:35 -0400

    1er avance del modulo de usuarios

--- api-panel/app/Http/Controllers/User/userController.php
+++ api-panel/app/Http/Controllers/User/userController.php
@@ -10,9 +10,25 @@ class userController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $search = $request->get("search");
+        $users = User::where("name", "LIKE", "%" . $search . "%")->orderBy("id", "desc")->get();
+        return response()->json([
+            "users" => $users->map(function ($user) {
+                return [
+                    "id" => $user->id,
+                    "full_name" => $user->name . " " . $user->surname,
+                    "email" => $user->email,
+                    "avatar" => $user->avatar,
+                    "created_at" => $user->created_at->format("Y/m/d H:i:s"),
+                    "role_id" => $user->role_id,
+                    "role" => [
+                        "name" => $user->role->name,
+                    ],
+                ];
+            }),
+        ], 200);
     }
 
     /**
@@ -20,7 +36,7 @@ class userController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
